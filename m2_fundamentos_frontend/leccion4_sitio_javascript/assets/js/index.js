79ff380a2513140156aa0d0b4936ba47efda9e5a
Rodrigo Pereira <rodrigo.alonso.pereira@gmail.com>
2025-11-13 16:38:47 -0300

    WIP: Sitio con js

---
+++ m2_fundamentos_frontend/leccion4_sitio_javascript/assets/js/index.js
@@ -0,0 +1,24 @@
+/*
+console.log("¡Hola, mundo!");
+let nombre = "Rodrigo";
+console.log("Mi nombre es " + nombre);
+document.getElementById("saludo").innerHTML = "¡Hola, " + nombre + "! Bienvenido a mi sitio web.";
+*/
+
+function mostrarFechaActualizable() {
+    let nombre = "Rodrigo";
+    let fecha = new Date();
+    // El uso de ` ` (template literals) para insertar variables en la cadena
+    document.getElementById('saludo').innerHTML = `Hola, ${nombre}.`;
+    document.getElementById('fecha').innerHTML = `Hola, ${nombre}. 
+    La fecha y hora actual es: ${fecha.toLocaleString()}`;
+
+    // Llama a la función de nuevo después de 1 segundo (1000 ms)
+    // setInterval() es otra forma de hacerlo
+    setTimeout(mostrarFechaActualizable, 1000);
+    /*
+    setInterval(function() {
+        mostrarFechaActualizable()
+    }, 1000);
+    */
+}
