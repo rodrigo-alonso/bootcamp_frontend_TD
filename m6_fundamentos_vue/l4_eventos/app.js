d2a4a9318adf4bc720b39075ea411a7bbc6b510e
Rodrigo Pereira <rodrigo.alonso.pereira@gmail.com>
2026-02-10 16:43:05 -0300

    ADD: Aplicativo que registra diferentes eventos

--- m6_fundamentos_vue/l4_eventos/app.js
+++ m6_fundamentos_vue/l4_eventos/app.js
@@ -1,5 +1,5 @@
 // Composition API 
-const {createApp, ref} = Vue;
+const { createApp, ref } = Vue;
 
 // Implementacion escencial de Composition API
 // createApp({
@@ -19,27 +19,53 @@ createApp({
 
         const registrarEvento = (tipo) => {
             eventos.value.unshift(
-                `Evento: ${tipo} - ${new Date().toLocaleDateString()}`
+                `Evento: ${tipo} - ${new Date().toLocaleTimeString()}`
             )
         }
 
         const registrarTecla = (e) => {
             eventos.value.unshift(
-                `Tecla: ${e.key} (${e.type}) - ${new Date().toLocaleDateString()}`
+                `Tecla: ${e.key} (${e.type}) - ${new Date().toLocaleTimeString()}`
             )
         }
 
         const registrarEventoInput = (e) => {
             eventos.value.unshift(
-                `Input: ${e.target.value}`
+                `Input: ${e.target.value} - ${new Date().toLocaleTimeString()}`
             )
         }
 
-        return{
+        const enviarFormulario = (e) => {
+            registrarEvento(`Submit formulario (Nombre: ${nombre.value}, Pais: ${pais.value})`);
+            nombre.value = ''; // Limpia el input del nombre
+            pais.value = ''; // Limpia el input del pais
+        }
+
+        const seleccionarTodo = (e) => {
+            e.preventDefault(); // Evento x defecto prevenido
+            e.target.select(); // Evento que permite seleccionar todo el texto
+            eventos.value.unshift( // Evento que muestra el mensaje de que se ha seleccionado el texto
+                `CTRL + m -> texto seleccionado - ${new Date().toLocaleTimeString()}`
+            );
+        }
+
+        // Se usa cuando se selecciona un combobox, radiobutton, select
+        const eventoChange = (e) => {
+            eventos.value.unshift(
+                `Change: ${e.target.value} - ${new Date().toLocaleTimeString()}` //Selecciona el value elejido x usuario y lo muestra
+            )
+        }
+
+        return {
             eventos,
+            nombre,
+            pais,
             registrarEvento,
             registrarTecla,
-            registrarEventoInput
+            registrarEventoInput,
+            enviarFormulario,
+            seleccionarTodo,
+            eventoChange
         }
     },
 }).mount('#app');
