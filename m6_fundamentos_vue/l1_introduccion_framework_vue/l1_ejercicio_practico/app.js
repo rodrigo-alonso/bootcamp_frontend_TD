3333ab5a5c96a25d637a0dc3f654c347973df88d
Rodrigo Pereira <rodrigo.alonso.pereira@gmail.com>
2026-02-27 15:23:59 -0300

    WIP: Tarea leccion 1 modulo 1

--- m6_fundamentos_vue/l1_introduccion_framework_vue/l1_ejercicio_practico/app.js
+++ m6_fundamentos_vue/l1_introduccion_framework_vue/l1_ejercicio_practico/app.js
@@ -1,28 +1,10 @@
-// Option API
+// Composition API
 
-const { createApp } = Vue;
+const { createApp, ref } = Vue;
 
 createApp({
-    data () {
-        return {
-            nombre: '',
-            datosJson: null
-        }
+    setup() {
+        const personajes = ref([]);
+        
     },
-    async mounted() {
-        try {
-            const response = await fetch('info.json')
-            this.datosJson = await response.json();
-            console.log(this.datosJson)
-        } catch (error) {
-            console.error("Error al leer el JSON")
-        }
-    },
-
-    methods: {
-        cargarPirata() {
-            const data = datosJson;
-            
-        }
-    }
 }).mount('#app');
