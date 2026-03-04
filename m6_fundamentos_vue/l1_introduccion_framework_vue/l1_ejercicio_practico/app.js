9cf1d4d77671ba7682203e698cc4763c33991a38
Rodrigo Pereira <rodrigo.alonso.pereira@gmail.com>
2026-03-04 20:20:06 -0300

    ADD: Ejericio practico modulo 6, leccion 1

--- m6_fundamentos_vue/l1_introduccion_framework_vue/l1_ejercicio_practico/app.js
+++ m6_fundamentos_vue/l1_introduccion_framework_vue/l1_ejercicio_practico/app.js
@@ -1,10 +1,31 @@
 // Composition API
 
-const { createApp, ref } = Vue;
+const { createApp, ref, computed, onMounted } = Vue;
 
 createApp({
     setup() {
-        const personajes = ref([]);
+        const personajes = ref([]); // Variable reactiva
+
+        // Función para obtener los personajes desde el archivo JSON
+        const fetchPersonajes = async () => {
+            const response = await fetch('./info.json');
+            const data = await response.json();
+            personajes.value = data.personajes;
+        };
+
+        const frutaDiablo = computed((p) => {
+            if (!p.fruta_del_diablo.tiene_fruta) return 'No tiene fruta del diablo';
+            return p.fruta_del_diablo.nombre;
+        });
+
+        // Hook para ejecutar la función al montar el componente
+        onMounted(() => {
+            fetchPersonajes();
+        });
         
+        return {
+            personajes,
+            frutaDiablo
+        };
     },
 }).mount('#app');
